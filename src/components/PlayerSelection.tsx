30aed9c0686073acff281ae3f05567f02dc8dc2e
lukehalko <halko@udel.edu>
2021-09-27 15:30:16 -0400

    fixed player selection bug, added util folder for helper functions

--- src/components/PlayerSelection.tsx
+++ src/components/PlayerSelection.tsx
@@ -1,21 +1,16 @@
 import { Button, Col } from 'react-bootstrap'
 import { Player } from '../interfaces/player'
 import PLAYERS from '../assets/players.json';
-
+import { getRandomElement } from '../util/data';
+var i = 0;
 export function PlayerSelection({setPlayer}: {setPlayer: (p: Player)=>void}): JSX.Element {
-    var i=0;
     return(
         <Col>
         <h1>Select Player</h1>
-        <form>
-            <label>
-                Name (this doesn't work right now):
-                <input type="text" name="name" />
-            </label>
-                <input type="submit" value="Submit" />
-        </form>
-        {setPlayer}
-        <Button onClick={() => setPlayer(PLAYERS[i++] as Player)}>Change Player</Button>
+        {setPlayer /*why is this here*/}
+        <Button onClick={() =>
+            setPlayer(PLAYERS[i++] as Player)
+            }>Change Player</Button>
         </Col>
     )
 }
